0f46479712c69d4f92577a3ccd8805ccb2f910fe
calypso <li5up6@gmail.com>
2018-07-27 14:57:16 -0400

    Calypso: implemented basic framework for patching kips

--- src/kip.h
+++ src/kip.h
@@ -1,6 +1,9 @@
 #ifndef _KIP_H_
 #define _KIP_H_
 
+#include "hwinit/list.h"
+#include "hwinit/sha256.h"
+
 typedef struct kip_patch
 {
 	u32 off; //absolute decompressed offset
@@ -42,7 +45,29 @@ static kip_id kip_id_list[] =
 {
 	{ "FS", "\xde\x9f\xdd\xa4\x08\x5d\xd5\xfe\x68\xdc\xb2\x0b\x41\x09\x5b\xb4", fs_patch_100 }, // FS 1.0.0
 	{ "FS", "\xfc\x3e\x80\x99\x1d\xca\x17\x96\x4a\x12\x1f\x04\xb6\x1b\x17\x5e", fs_patch_100 }, // FS 1.0.0 "exfat"
+	{ NULL, NULL, NULL}
 };
+
+int patch_kips(link_t *info) {
+
+	LIST_FOREACH_ENTRY(pkg2_kip1_info_t, ki, info, link) {
+		struct sha256_state sha_ctx;
+		u8 kip_hash[0x20];
+		sha256_init(&sha_ctx);
+		sha256_update(&sha_ctx, (u8 *)ki->kip1->data, ki->size);
+		sha256_finalize(&sha_ctx);
+		sha256_finish(&sha_ctx, kip_hash);
+		int i=0;
+		while(kip_id_list[i].name!=NULL) {
+			if(!strcmp(kip_id_list[i].name, ki->kip1->name)) {
+				if(!memcmp(kip_id_list[i].sha, kip_hash, 0x10)) {
+					//hashes are the same
+				}
+			}
+		}
+		
+	}
+}
 
 
 #endif
